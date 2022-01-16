87c5657c5fb780b72dd9e0a93a3848aa662112ef
ndhuy <ndhuy@fit.hcmus.edu.vn>
2022-01-16 17:51:07 +0700

    feat(lab04): create Add new Form

---
+++ lab04/src/components/post/postForm.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import './style.css';
+
+export const PostForm = () => {
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+
+  const onTitleChanged = e => setTitle(e.target.value);
+  const onContentChanged = e => setContent(e.target.value);
+
+  return (
+    <section>
+      <h2>Add a new Post</h2>
+      <form>
+        <label htmlFor='postTitle'>Post title:</label>
+        <input type='text' id='postTitle' name='postTitle' value={title} onChange={onTitleChanged} />
+        <label htmlFor='postContent'>Post content</label>       
+        <textarea id='postContent' name='postContent' value={content} onChange={onContentChanged} />
+        <button type='button'>Create new Post</button>
+      </form>
+    </section>
+  );
+}
